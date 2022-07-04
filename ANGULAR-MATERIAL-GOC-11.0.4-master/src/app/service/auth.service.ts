694105146410c0969ad1b71d35e80c5c4142543a
origin <hai@gmail.com>
2022-07-04 15:14:31 +0700

    done

---
+++ ANGULAR-MATERIAL-GOC-11.0.4-master/src/app/service/auth.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment.prod';
+import {SignUpForm} from '../model/SignUpForm';
+import {Observable} from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    private API_SIGNUP = environment.API_LOCAL + 'signup';
+
+
+    constructor(private http: HttpClient) {}
+    signUp(signUp: SignUpForm): Observable<any>{
+        return this.http.post<any>(this.API_SIGNUP, signUp);
+    }
+}
